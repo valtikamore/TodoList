e725d077da3611a96e9c3f11dc66721ac634ac54
Valentine <valtikaless@gmail.com>
2021-04-08 23:09:04 +0300

    10-4 app/appUseReducer.text && refactor

--- src/state/redux/store.ts
+++ src/state/redux/store.ts
@@ -20,7 +20,6 @@ export const  store = createStore(
 export type AppRootStateType = ReturnType<typeof rootReducer>
 
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
-// @ts-ignore
 
 
 
